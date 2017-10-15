c38e46d1f85fc304d4fb15a61464aa5ed04cd98b
wuwenjun <johnwu1016@gmail.com>
2017-10-15 14:46:55 -0700

    initial commit

---
+++ DecisionTree.py
@@ -0,0 +1,180 @@
+#!/usr/bin/env python3
+import sys
+import random
+import numpy as np
+from sklearn.model_selection import KFold
+# from sklearn import tree
+from collections import Counter
+
+
+# Class for instances with operations
+class Instances(object):
+    def __init__(self):
+        self.label = []
+        self.attrs = []
+        self.num_attrs = -1
+        self.num_instances = 0
+        self.attr_set = []
+        self.dic = {'republican':0, 'democrat':1, 'n':0, 'y':1, '?':2,
+                    'positive':0, 'negative':1, 'o':0, 'x':1, 'b':2}
+
+    def add_instance(self, _lbl, _attrs):
+        self.label.append(_lbl)
+        self.attrs.append(_attrs)
+        if self.num_attrs == -1:
+            self.num_attrs = len(_attrs)
+        else:
+            assert (self.num_attrs == len(_attrs))
+        self.num_instances += 1
+        assert (self.num_instances == len(self.label))
+
+    def make_attr_set(self):
+        self.attr_set = [set([self.attrs[i][j] for i in range(self.num_instances)]) for j in range(self.num_attrs)]
+
+    def load_file(self, file_name):
+        with open(file_name, 'r') as f:
+            for line in f:
+                data = line.strip().split(',')
+                self.add_instance(self.dic[data[0]], [self.dic[x] for x in data[1:]])
+        self.make_attr_set()
+        return self
+
+    def split(self, att_idx):
+        assert (0 <= att_idx < self.num_attrs)
+        split_data = {x: Instances() for x in self.attr_set[att_idx]}
+        for i in range(self.num_instances):
+            key = self.attrs[i][att_idx]
+            split_data[key].add_instance(self.label[i], self.attrs[i])
+        for key in split_data:
+            split_data[key].attr_set = self.attr_set
+        return split_data
+
+    def shuffle(self):
+        indices = list(range(len(self.label)))
+        random.shuffle(indices)
+        res = Instances()
+        for x in indices:
+            res.add_instance(self.label[x], self.attrs[x])
+        res.attr_set = self.attr_set
+        return res
+
+    def get_subset(self, keys):
+        res = Instances()
+        for x in keys:
+            res.add_instance(self.label[x], self.attrs[x])
+        res.attr_set = self.attr_set
+        return res
+
+
+
+def compute_entropy(data: Instances):
+    total_entropy = 0.0
+    ########## Please Fill Missing Lines Here ##########
+    x = Counter(data.label).most_common()[0][1]
+    if x == data.num_instances:
+        return 0
+    a = x / data.num_instances
+    b = (data.num_instances - x) / data.num_instances
+    total_entropy = -a * np.log(a) - b * np.log(b)
+    return total_entropy
+
+
+def compute_info_gain(data: Instances, att_idx):
+    info_gain = 0.0
+    ########## Please Fill Missing Lines Here ##########
+
+    old = compute_entropy(data)
+    n = data.num_instances
+    splits = data.split(att_idx)
+    new = np.sum([compute_entropy(x) * x.num_instances / n for x in splits.values() if x.num_instances])
+    info_gain = old - new
+    return info_gain
+
+
+def comput_gain_ratio(data, att_idx):
+    gain_ratio = 0.0
+    ########## Please Fill Missing Lines Here ##########
+
+    i = compute_info_gain(data, att_idx)
+    if i < 1e-9:
+        return 0
+
+    n = data.num_instances
+    splits = data.split(att_idx)
+    info = -np.sum([x.num_instances * np.log(x.num_instances / n) / n for x in splits.values() if x.num_instances])
+    gain_ratio = i / info
+    return gain_ratio
+
+
+# Class of the decision tree model based on the ID3 algorithm
+class DecisionTree(object):
+    def __init__(self, _instances, _sel_func):
+        self.instances = _instances
+        self.sel_func = _sel_func
+        self.gain_function = compute_info_gain if _sel_func == 0 else comput_gain_ratio
+        self.m_attr_idx = None  # The decision attribute if the node is a branch
+        self.m_class = None  # The decision class if the node is a leaf
+        self.make_tree()
+
+    def make_tree(self):
+        if self.instances.num_instances == 0:
+            # No any instance for this node
+            self.m_class = '**MISSING**'
+        else:
+            gains = [self.gain_function(self.instances, i) for i in range(self.instances.num_attrs)]
+            self.m_attr_idx = np.argmax(gains)
+            if np.abs(gains[self.m_attr_idx]) < 1e-9:
+                # A leaf to decide the decided class
+                self.m_attr_idx = None
+                ########## Please Fill Missing Lines Here ##########
+                self.m_class = Counter(self.instances.label).most_common(1)[0][0]
+            else:
+                # A branch
+                split_data = self.instances.split(self.m_attr_idx)
+                self.m_successors = {x: DecisionTree(split_data[x], self.sel_func) for x in split_data}
+                for x in self.m_successors:
+                    self.m_successors[x].make_tree()
+
+    def classify(self, attrs):
+        assert ((self.m_attr_idx != None) or (self.m_class != None))
+        if self.m_attr_idx == None:
+            return self.m_class
+        else:
+            return self.m_successors[attrs[self.m_attr_idx]].classify(attrs)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 1 + 1:
+        print('--usage python3 %s data [0/1, 0-Information Gain, 1-Gain Ratio, default: 0]' % sys.argv[0],
+              file=sys.stderr)
+        sys.exit(0)
+    random.seed(27145)
+    np.random.seed(27145)
+
+    sel_func = int(sys.argv[2]) if len(sys.argv) > 1 + 1 else 0
+    assert (0 <= sel_func <= 1)
+
+    data = Instances().load_file(sys.argv[1])
+    data = data.shuffle()
+
+    # 5-Fold CV
+    kf = KFold(n_splits=5)
+    n_fold = 0
+    accuracy = []
+    for train_keys, test_keys in kf.split(range(data.num_instances)):
+        train_data = data.get_subset(train_keys)
+        test_data = data.get_subset(test_keys)
+        n_fold += 1
+        # clf = tree.DecisionTreeClassifier()
+        # clf.fit(train_data.attrs, train_data.label)
+        # predictions = clf.predict(test_data.attrs)
+        # benchmark tests
+        model = DecisionTree(train_data, sel_func)
+        predictions = [model.classify(test_data.attrs[i]) for i in range(test_data.num_instances)]
+        num_correct_predictions = sum(
+            [1 if predictions[i] == test_data.label[i] else 0 for i in range(test_data.num_instances)])
+        nfold_acc = float(num_correct_predictions) / float(test_data.num_instances)
+        accuracy.append(nfold_acc)
+        print('Fold-{}: {}'.format(n_fold, nfold_acc))
+
+    print('5-CV Accuracy = {}'.format(np.mean(accuracy)))
---
+++ try.py
@@ -0,0 +1,7 @@
+import math
+
+a = {'1':-1, '2': -2}
+b = ['1', '2']
+print([a[x] for x in b])
+
+print(1/2)
